1d92f07cb880058445c4b4a7b9072dbe50c43b3b
Emly <emily_foulkes@hotmail.com>
2022-04-25 15:55:29 +0100

    make kudos update straight away

--- lib/kudos/kudos_board.dart
+++ lib/kudos/kudos_board.dart
@@ -1,6 +1,8 @@
 import 'package:careshare/kudos/kudos_board_widget.dart';
+import 'package:careshare/profile_manager/cubit/profile_cubit.dart';
 import 'package:careshare/profile_manager/models/profile.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class KudosBoard extends StatelessWidget {
   final List<Profile> profileList;
@@ -12,13 +14,17 @@ class KudosBoard extends StatelessWidget {
     _profileList.sort(
       (a, b) => b.kudos.compareTo(a.kudos),
     );
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: Row(
-        children: _profileList
-            .map((Profile profile) => KudosBoardWidget(profile: profile))
-            .toList(),
-      ),
+    return BlocBuilder<ProfileCubit, ProfileState>(
+      builder: (context, state) {
+        return SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: _profileList
+                .map((Profile profile) => KudosBoardWidget(profile: profile))
+                .toList(),
+          ),
+        );
+      },
     );
   }
 }
