0edf176b1f7b709e619306e0d19452597e1537b0
Emly <emily_foulkes@hotmail.com>
2022-04-25 21:55:24 +0100

    improve comments

--- lib/task_manager/presenter/task_detailed_view/widgets/display_comments_widget.dart
+++ lib/task_manager/presenter/task_detailed_view/widgets/display_comments_widget.dart
@@ -5,6 +5,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
 
+import '../../../../profile_manager/cubit/profile_cubit.dart';
+
 class DisplayCommentsWidget extends StatelessWidget {
   final CareTask task;
   const DisplayCommentsWidget({Key? key, required this.task}) : super(key: key);
@@ -22,32 +24,40 @@ class DisplayCommentsWidget extends StatelessWidget {
                 const Text('Comments'),
                 if (task.comments != null)
                   Column(
-                    children: task.comments!
-                        .map(
-                          (comment) => Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Card(
-                              child: Container(
-                                  width: double.infinity,
-                                  padding: const EdgeInsets.all(8),
-                                  decoration: BoxDecoration(
-                                    color: Colors.green[100],
+                    children: task.comments!.map((comment) {
+                      final bool isCommentByMe = comment.createdBy ==
+                          BlocProvider.of<ProfileCubit>(context).myProfile.id;
+                      return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                              width: double.infinity,
+                              padding: const EdgeInsets.all(8),
+                              decoration: BoxDecoration(
+                                color: isCommentByMe
+                                    ? Colors.blue[100]
+                                    : Colors.green[100],
+                                borderRadius: BorderRadius.only(
+                                  topLeft: const Radius.circular(12),
+                                  bottomLeft: isCommentByMe
+                                      ? const Radius.circular(12)
+                                      : const Radius.circular(0),
+                                  topRight: const Radius.circular(12),
+                                  bottomRight: isCommentByMe
+                                      ? const Radius.circular(0)
+                                      : const Radius.circular(12),
+                                ),
+                              ),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    '${comment.createdByDisplayName} on ${DateFormat('E d MMM yyyy').add_jm().format(comment.dateCreated)}',
+                                    style: Theme.of(context).textTheme.caption,
                                   ),
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                          '${comment.commment} ${comment.createdByDisplayName} on ${DateFormat('E d MMM yyyy').add_jm().format(comment.dateCreated)}',
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .caption),
-                                    ],
-                                  )),
-                            ),
-                          ),
-                        )
-                        .toList(),
+                                  Text(comment.commment),
+                                ],
+                              )));
+                    }).toList(),
                   ),
                 Center(
                   child: AddCommentWidget(task: task),
